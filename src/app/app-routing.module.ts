ddd28710af817b79c3db12db303a55ac58103773
WalaaKha <w.abuhasanien@gmail.com>
2018-12-19 06:56:01 +0200

    display department details in seperate component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {ContactUsComponent} from './contact-us/contact-us.component';
+import {DepartmentComponent} from './department/department.component';
 import {DepartmentDescriptionComponent} from './department-description/department-description.component';
 
 const routes: Routes = [
   {path: 'about', component:AboutComponent},
   {path: 'contact', component:ContactUsComponent},
+  {path: 'departments', component:DepartmentComponent},
   {path: 'departments/:id', component:DepartmentDescriptionComponent}
 ];
 
